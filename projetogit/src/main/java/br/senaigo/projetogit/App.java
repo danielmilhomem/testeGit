13e44b31561e75c012080459ef4e2694df055611
danielmilhomem <snake_danielmilhomem@hotmail.com>
2019-03-14 21:37:42 -0300

    meu primerio commit

---
+++ projetogit/src/main/java/br/senaigo/projetogit/App.java
@@ -0,0 +1,13 @@
+package br.senaigo.projetogit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!---\0/" );
+    }
+}
